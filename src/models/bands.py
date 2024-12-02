840e50b9936524dfcca3fb13f2b463a7c992d4c1
simizzz <merander207203@gmail.com>
2024-12-02 22:23:03 +0500

    add m2m fields

--- src/models/bands.py
+++ src/models/bands.py
@@ -1,4 +1,5 @@
-from piccolo.columns import ForeignKey
+from piccolo.columns import ForeignKey, LazyTableReference
+from piccolo.columns.m2m import M2M
 from piccolo.table import Table
 from piccolo import columns
 from .users import User
@@ -10,6 +11,7 @@ class Band(Table, tablename="band"):
     leader = ForeignKey(User, related_name="leader_bands")
     name = columns.Varchar()
     description = columns.Text(null=True)
+    members = M2M(LazyTableReference("user_bands", module_path=__name__))
 
 
 class UserBands(Table, tablename="user_band"):
--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,7 @@
+from piccolo.columns import LazyTableReference
 from piccolo.table import Table
 from piccolo import columns
+from piccolo.columns.m2m import M2M
 import uuid
 
 from services.auth.types import UserRole
@@ -13,3 +15,4 @@ class User(Table, tablename="user"):
     role = columns.Varchar(choices=UserRole, default=UserRole.CLIENT)
     description = columns.Text(null=True)
     password = columns.Secret(length=255)
+    bands = M2M(LazyTableReference("user_bands", module_path=__name__))
